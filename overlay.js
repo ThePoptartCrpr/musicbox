11a5bfabea735dab90e0dd02ada3c7cd862045de
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2019-01-05 15:46:24 -0800

    Started designing new panel system

--- overlay.js
+++ overlay.js
@@ -1,5 +1,11 @@
 function MusicBoxOverlayGui(musicbox) {
   this.musicbox = musicbox;
+  
+  // Create panels
+  this.utilityTray = new UtilityTrayPanel();
+  this.songManager = new SongManagerPanel();
+  this.trackListing = new TrackListingPanel();
+  this.playlistSelection = new PlaylistSelectionPanel();
 
   // GUI values
   this.hovered = false;
@@ -7,30 +13,87 @@ function MusicBoxOverlayGui(musicbox) {
 
   // Draw values
   this.color = [170, 0, 0, 100];
+  
+  
+  this.click = function() {
+    
+  }
 
   this.tick = function() {
-
+    this.utilityTray.tick();
+    this.songManager.tick();
+    this.trackListing.tick();
+    this.playlistSelection.tick();
   }
 
   this.draw = function() {
-    Renderer.drawRect(
-      Renderer.color(this.color[0], this.color[1], this.color[2], this.color[3]),
-      Renderer.screen.getWidth() - 20,
-      10,
-      3,
-      40
-    )
-
-    Renderer.drawRect(
-      Renderer.color(0, 0, 0, this.color[3]),
-      Renderer.screen.getWidth() - 17,
-      10,
-      17,
-      40
-    )
+    this.utilityTray.draw();
+    this.songManager.draw();
+    this.trackListing.draw();
+    this.playlistSelection.draw();
   }
+  
+  
+  // Panels
+  function UtilityTrayPanel() {
+    this.tick = function() {
+      
+    }
+    
+    this.draw = function() {
+      
+    }
+  }
+  
+  function SongManagerPanel() {
+    this.tick = function() {
+      
+    }
+    
+    this.draw = function() {
+      Renderer.drawRect(
+        Renderer.color(this.color[0], this.color[1], this.color[2], this.color[3]),
+        Renderer.screen.getWidth() - 20,
+        10,
+        3,
+        40
+      )
+  
+      Renderer.drawRect(
+        Renderer.color(0, 0, 0, this.color[3]),
+        Renderer.screen.getWidth() - 17,
+        10,
+        17,
+        40
+      )
+    }
+  }
+  
+  function TrackListingPanel() {
+    this.tick = function() {
+      
+    }
+    
+    this.draw = function() {
+      
+    }
+  }
+  
+  function PlaylistSelectionPanel() {
+    this.tick = function() {
+      
+    }
+    
+    this.draw = function() {
+      
+    }
+  }
+  
+  // Elements
+  
 }
 
+// Tick and draw gui
 register('step', function() {
   MusicBox.getGui().tick();
 }).setFps(30);
